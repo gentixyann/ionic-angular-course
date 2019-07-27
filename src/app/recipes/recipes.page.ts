55eadad6af028317a75cce61912ebc7b4fc90285
Gento <a551.return@gmail.com>
2019-07-27 20:49:18 +1200

    routerLink

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
@@ -7,24 +8,13 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-  recipes: Recipe[] = [
-    {
-      id: 'r1',
-      title: 'Curry',
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmWgOEr6yUvt_b0_5rn4UIkcos6zQTgnQIjl8V9Vnc49ARyn5cSA',
-      ingredients: ['rice', 'meat', 'potato', 'carrot']
-    },
-    {
-      id: 'r2',
-      title: 'Spaghetti',
-      imageUrl: 'https://c-chefgohan.gnst.jp/imgdata/recipe/30/22/2230/rc732x546_1403111105_668ffdb6d7b3f055db0e2340fd9d727d.jpg',
-      ingredients: ['Spaghetti', 'meat', 'soy', 'carrot'] 
-    }
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+
+  constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
+    this.recipes = this.recipesService.getAllRecipes();
   }
 
 }
