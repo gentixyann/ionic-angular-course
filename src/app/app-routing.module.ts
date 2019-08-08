7f9d23443fc8db8b2b37bb29d5c80c6326052200
Gento <a551.return@gmail.com>
2019-08-08 20:28:55 +1200

    completed demo auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
+import { AuthGuard } from './auth/auth.guard';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'places', pathMatch: 'full' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
-  { path: 'bookings', loadChildren: './bookings/bookings.module#BookingsPageModule' },
-  { path: 'places', loadChildren: './places/places.module#PlacesPageModule' },
+  { 
+    path: 'bookings',
+    loadChildren: './bookings/bookings.module#BookingsPageModule', canLoad: [AuthGuard]
+   },
+  { 
+    path: 'places',
+    loadChildren: './places/places.module#PlacesPageModule', canLoad: [AuthGuard]
+   },
 ];
 
 @NgModule({
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,12 +1,23 @@
+import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment } from '@angular/router';
+import { CanLoad, Route, UrlSegment, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
-  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+  constructor(
+    private authService: AuthService,
+    private router: Router)  {}
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+    ): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.authService.userIsAuthenticated) {
+      this.router.navigateByUrl('/auth');
+    }
+    return this.authService.userIsAuthenticated;
   }
 }
